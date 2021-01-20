76292a3c029b8b8a109dc03f3989a2354fa92406
Alena Khineika <alena.khineika@gmail.com>
2021-01-20 17:09:33 +0100

    feat: fetch all tweets

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Twitter = require('twitter');
+const util = require('util');
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
@@ -11,18 +12,36 @@ const client = new Twitter({
   access_token_secret: process.env.TWITTER_AUTHENTICATION_ACCESS_TOKEN_SECRET,
 });
 
-const getColorOfBerlin = () => {
-  client.get('statuses/user_timeline', { screen_name: 'colorofberlin', count: 200 }, function(error, tweets, response) {
-    console.log('error----------------------');
-    console.log(error);
-    console.log('----------------------');
-    console.log('tweets----------------------');
-    console.log(JSON.stringify(tweets, null, 2));
-    console.log('----------------------');
-    console.log('length----------------------');
-    console.log(tweets.length);
-    console.log('----------------------');
-  });
+const fetchTweetsForPage = async (page) => {
+  const getTweets = util.promisify(
+    client.get.bind(client)
+  );
+
+  return await getTweets('statuses/user_timeline', { screen_name: 'colorofberlin', count: 200, page });
 }
 
-getColorOfBerlin();
+const getColorsOfBerlin = async (numberOfPages) => {
+  let tweets = [];
+
+  for (let i = 1; i <= numberOfPages; i++) {
+    const tweetsPerPage = await fetchTweetsForPage(i);
+
+    if (tweetsPerPage.length > 0) {
+      tweets = tweets.concat(tweetsPerPage);
+    } else {
+      return tweets;
+    }
+  };
+}
+
+(async () => {
+  try {
+    const colors = await getColorsOfBerlin(100);
+
+    console.log('length----------------------');
+    console.log(colors.length);
+    console.log('----------------------');
+  } catch (error) {
+    console.error(error);
+  }
+})();
