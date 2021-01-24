640ec1829f845fe862e2d7fe13746940a18614a6
Alena Khineika <alena.khineika@gmail.com>
2021-01-24 18:35:27 +0100

    docs: add comments to code.

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -15,6 +15,10 @@ interface AppState {
 export default class App extends React.Component<{}, AppState> {
   state: AppState = { tweets: [] };
 
+  // When a user clicks the `Get Tweets` button
+  // Send a request to server/index.ts and wait for results.
+  // After receiving results update the React state
+  // To fire page rendering.
   getText = (): void => {
     fetch(apiRoute.getRoute('getTweets'))
       .then(res => res.json())
@@ -24,6 +28,8 @@ export default class App extends React.Component<{}, AppState> {
   getWeekOfMonth(offset: number): JSX.Element {
     const blocks: JSX.Element[] = [];
 
+    // All days have the same css, therefore we use a loop
+    // to create only one div and use it for all days.
     for (let day = 0; day < 6; day++) {
       blocks.push(<div className="day" style={{background: this.state.tweets[offset + day].colorHex}}></div>);
     }
@@ -31,18 +37,25 @@ export default class App extends React.Component<{}, AppState> {
     return (<div className="week">{blocks}</div>);
   }
 
+  // Returns layout for a month.
   getMonthPalette(): JSX.Element {
-    const rows: any[] = [];
+    const rows: JSX.Element[] = [];
     let offset = 0;
 
-    for (let week = 0; week < 5; week++) {
+    // One month contains 4 weeks, therefore we use a loop to not repeat the same code for each week.
+    for (let week = 0; week < 4; week++) {
       rows.push(this.getWeekOfMonth(offset));
-      offset += 6;
+      offset += 7;
     }
 
     return (<div className="month">{rows}</div>);
   }
 
+  // Listens for this.state.tweets changes and for each change renders the page
+  // to reflect these changes. Here you can specify different page layouts
+  // depending on the data format received from the server.
+  // Currently it checks if the server returns 30 tweets
+  // It renders the layout with 30 squares filled with a color.
   render(): JSX.Element {
     const tweets = this.state.tweets;
 
--- src/server/index.ts
+++ src/server/index.ts
@@ -10,6 +10,7 @@ const MONGODB_URI = 'mongodb://localhost';
 const MONGODB_DATABASE = 'colorofberlin';
 const MONGODB_COLLECTION = 'tweets';
 
+// Instantiate a MongoDB client.
 const client: MongoClient = new MongoClient(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -19,9 +20,9 @@ let collection: Collection | undefined;
 
 (async () => {
   try {
-    await client.connect();
-    database = client.db(MONGODB_DATABASE);
-    collection = database.collection(MONGODB_COLLECTION);
+    await client.connect(); // Connect to mongodb://localhost.
+    database = client.db(MONGODB_DATABASE); // Access the `colorofberlin` database.
+    collection = database.collection(MONGODB_COLLECTION); // Access the `tweets` collection.
   } catch (error) {
     console.error(`Connect to MongoDB failed: ${error.message}`);
   }
@@ -36,7 +37,9 @@ app.get('/', (req, res) => res.sendFile(CLIENT_PUBLIC_HTML));
 
 const router = express.Router();
 
+// Listen for requests from the client and for `getTweets` request return 30 recent tweets.
 router.get('/getTweets', async (req: Request, res: Response) => {
+  // The data format expected by the client.
   let tweets: {
     id: number,
     created_at: string,
@@ -46,13 +49,22 @@ router.get('/getTweets', async (req: Request, res: Response) => {
   }[] | [] = [];
 
   if (collection) {
+    // Use aggregation to find 30 recent tweets
+    // and modify the resulting array to match the expected data format.
     tweets = await collection.aggregate([
+      // Take the created_at field that has a string value and can't be sorted,
+      // and create a new convertedDate field that has a ISODate value.
       { $addFields: { convertedDate: { $toDate: '$created_at' } } },
+      // Sort descending from the most recent tweet to later ones.
       { $sort: { convertedDate: -1 } },
+      // Return only 30 recent tweets, not all the tweets that exist in the collection.
       { $limit: 30 },
+      // Return only id, created_at and text, not all the fields that a document has.
       { $project: { _id: 0, id: 1, created_at: 1, text: 1 } }
     ]).toArray();
 
+    // A color's hex value and a name are stored inside the text string.
+    // Parse the string to extract these values.
     tweets = tweets.map((item) => {
       const textAll = item.text.split('. #');
       const colorHex = `#${textAll[1].substring(0, 6)}`;
