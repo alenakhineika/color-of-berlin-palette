2827818345aa8e4d7780318b8ad0f71554bbe765
Alena Khineika <alena.khineika@gmail.com>
2021-01-26 16:02:46 +0100

    refactor: code formatting

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+
 import apiRoute from './router';
 
 import './app.less';
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { hydrate } from 'react-dom';
+
 import App from './app';
 
 hydrate(<App />, document.getElementById('root'));
