3ed437f61b982fe1c61ccc48fa2cb777ef103118
Alena Khineika <alena.khineika@gmail.com>
2023-06-15 17:56:53 +0200

    feat: build leaderboard with plain d3

--- src/client/components/allData.tsx
+++ src/client/components/allData.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import type { Document } from 'mongodb';
 
-interface Proprs {
+interface Props {
   records: Document[];
 }
 
-export default class AllData extends React.Component<Proprs, {}> {
+export default class AllData extends React.Component<Props, {}> {
   renderRecordsPerYear(): React.ReactNode {
     return this.props.records.map((item, index) => {
       const dayGradient: string[] = [];
@@ -15,16 +15,23 @@ export default class AllData extends React.Component<Proprs, {}> {
       });
 
       return (
-        <div className="yearLine" key={`yearLine${index}`} title={`${item.day}`}>
-          <div className="yearItem" style={{
-            backgroundImage: `linear-gradient(${dayGradient.join(', ')})`
-          }} />
+        <div
+          className="yearLine"
+          key={`yearLine${index}`}
+          title={`${item.day}`}
+        >
+          <div
+            className="yearItem"
+            style={{
+              backgroundImage: `linear-gradient(${dayGradient.join(', ')})`,
+            }}
+          />
         </div>
       );
     });
   }
 
   render(): React.ReactNode {
-    return (<div className="year">{this.renderRecordsPerYear()}</div>);
+    return <div className="year">{this.renderRecordsPerYear()}</div>;
   }
 }
